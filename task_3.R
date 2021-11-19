b57a0eefbd5fcfde5b19f58b492acf5279b14138
Angelica Maria <a.alvarezb@uniandes.edu.co>
2021-11-18 19:52:41 -0500

    script y punto 3

---
+++ task_3.R
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+#############
+#  Punto 3  #
+#############
+
+
+# intial configuration
+rm(list = ls()) # limpia el entorno de R
+require(pacman)
+p_load(tidyverse,data.table,plyr,XML,rvest,xml2) # carga
+
+#3.1
+browseURL(url = 'https://es.wikipedia.org/wiki/Departamentos_de_Colombia',browser = getOption('browser'))
+
+html_wiki = "https://es.wikipedia.org/wiki/Departamentos_de_Colombia"
+html_wiki = read_html(html_wiki)
+class(html_wiki)
+
+#3.2
+titulo = html_wiki %>% html_nodes(xpath = '//*[@id="firstHeading"]') %>% html_text() # Convertir en texto
+titulo
+
+#3.3
+tabla_departamentos = html_wiki %>% html_nodes('table') 
+tabla_departamentos
+
+#econtramos la tabla que tiene los departamentos
+tabla_departamentos[4] %>% html_table(header = T,fill=T)
+
+departamentos = tabla_departamentos[4] %>% html_table(header = T,fill=T)  %>% as.data.frame()
